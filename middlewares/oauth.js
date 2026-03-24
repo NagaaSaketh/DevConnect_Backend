c067222587b14c36a830284d581f95303c390021
Saketh <vnaagsaketh8925@gmail.com>
2026-03-24 10:56:33 +0530

    chore:fixed oauth config

--- middlewares/oauth.js
+++ middlewares/oauth.js
@@ -1,8 +1,8 @@
-const verifyAccessToken = (req,res,next)=>{
-    if(!req.cookies.access_token){
-        return res.status(403).json({error:"Access denied"})
-    }
-    next();
-}
+const verifyAccessToken = (req, res, next) => {
+  if (!req.cookies.token) {
+    return res.status(403).json({ error: "Access denied" });
+  }
+  next();
+};
 
-module.exports = {verifyAccessToken};
+module.exports = { verifyAccessToken };
