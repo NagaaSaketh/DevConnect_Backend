defc0d1b950de90cdbb1c14bbe5dbb1e04807e7b
Saketh <vnaagsaketh8925@gmail.com>
2026-03-24 12:00:05 +0530

    chore:rollbacked to prev oauth config from main branch

--- middlewares/oauth.js
+++ middlewares/oauth.js
@@ -1,5 +1,5 @@
 const verifyAccessToken = (req, res, next) => {
-  if (!req.cookies.token) {
+  if (!req.cookies.acess_token) {
     return res.status(403).json({ error: "Access denied" });
   }
   next();
